b3e01722eee45bdf82c5dba55589421ab48fa72d
Yacin Tmimi <yacintmimi@gmail.com>
2018-06-11 05:19:27 -0400

    removed unnecessary function

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'options',
 ]
 
@@ -129,9 +130,15 @@ STATICFILES_DIRS = [
 ]
 
 
-
-
-
+#Rest Framework documentation
+# http://www.django-rest-framework.org/
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 
