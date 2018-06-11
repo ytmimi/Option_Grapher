9ab65011ee74c1d6bee3df4aee6a7970f6d986ab
Yacin Tmimi <yacintmimi@gmail.com>
2018-06-11 05:16:33 -0400

    added serializer to send Option_Model data to template

---
+++ mysite/options/serializers.py
@@ -0,0 +1,30 @@
+import json
+from options import models
+from options import payoff_functions as pf
+
+def option_chart_data():
+	option_list = []
+	x_values = [0,]
+	for option in models.Option_Model.objects.all():
+		option_list.append(
+			{'type':option.option_type,
+			'pos':option.position,
+			'strike':option.strike_price,
+			'price':option.traded_price,}
+			)
+		x_values.append(option.strike_price)
+		x_values.append(option.strike_price*2)
+
+	x_list = sorted(set(x_values))
+	payoff = {}
+	for x in x_list:
+		payoff[x] = 0 
+
+	for option in option_list:
+		for x in x_list:
+			payoff[x] = payoff[x] + pf.option_payoff(option['type'], option['pos'], option['strike'], option['price'], x)
+
+	data = {'x':[x for x in payoff.keys()], 
+			'y':[y for y in payoff.values()]}
+	print(data)
+	return json.dumps(data)
