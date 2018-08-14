4ba118a934eaa9d92e9a1d1ef86f9059ecbe6122
Yacin Tmimi <yacintmimi@gmail.com>
2018-08-14 11:55:38 -0400

    replaced key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nnsq%r!2q)t$=+)7fp)1nlojjxl&kf^aiv4#4jv+w7f%z7hcp4'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
